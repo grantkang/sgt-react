7165e6b70009105d37fc0f9fa2dabaa172c68bda
Grant Kang <3s.grantkang@gmail.com>
2020-04-17 21:32:25 -0700

    GradeTableComponent now shows a message when there are no grades

--- client/components/grade-table.jsx
+++ client/components/grade-table.jsx
@@ -2,25 +2,33 @@ import React from 'react';
 import GradeComponent from './grade';
 
 class GradeTableComponent extends React.Component {
-  render() {
 
-    const gradeElements = this.props.grades.map(grade =>
+  emptyTableMessage() {
+    const grades = this.props.grades;
+    return grades.length === 0 ? <span>No grades recorded</span> : null;
+  }
+
+  render() {
+    const grades = this.props.grades;
+    const gradeElements = grades.map(grade =>
       <GradeComponent key={grade.id} grade={grade} />
     );
-
     return (
-      <table className="table table-striped">
-        <thead className="thead-dark">
-          <tr>
-            <th scope="col">Student Name</th>
-            <th scope="col">Course</th>
-            <th scope="col">Grade</th>
-          </tr>
-        </thead>
-        <tbody>
-          {gradeElements}
-        </tbody>
-      </table>
+      <div>
+        <table className="table table-striped">
+          <thead className="thead-dark">
+            <tr>
+              <th scope="col">Student Name</th>
+              <th scope="col">Course</th>
+              <th scope="col">Grade</th>
+            </tr>
+          </thead>
+          <tbody>
+            {gradeElements}
+          </tbody>
+        </table>
+        {this.emptyTableMessage()}
+      </div>
     );
   }
 }
