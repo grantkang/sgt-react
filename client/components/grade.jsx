4c04d0c050336f605bc8de1339582dd020375651
Grant Kang <3s.grantkang@gmail.com>
2020-04-18 13:43:49 -0700

    added delete grade functionality

--- client/components/grade.jsx
+++ client/components/grade.jsx
@@ -3,11 +3,19 @@ import React from 'react';
 class GradeComponent extends React.Component {
   render() {
     const gradeData = this.props.grade;
+    const onDelete = this.props.onDelete;
     return (
       <tr>
         <td>{gradeData.name}</td>
         <td>{gradeData.course}</td>
         <td>{gradeData.grade}</td>
+        <td>
+          <button
+            className="btn btn-danger"
+            onClick={() => onDelete(gradeData.id)}>
+            Delete
+          </button>
+        </td>
       </tr>
     );
   }
